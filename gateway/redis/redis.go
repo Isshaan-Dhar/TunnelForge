40e51006da21a02003c63adbc30b5afeed6baa62
Ishaan Dhar <ishaan.dhar2004@gmail.com>
2026-07-07 20:53:44 +0530

    Update redis.go

--- gateway/redis/redis.go
+++ gateway/redis/redis.go
@@ -11,15 +11,6 @@ type Store struct {
 	client *redisclient.Client
 }
 
-var rateLimitScript = redisclient.NewScript(`
-	local current
-	current = redis.call("incr", KEYS[1])
-	if current == 1 then
-		redis.call("expire", KEYS[1], ARGV[1])
-	end
-	return current
-`)
-
 func New(addr string) (*Store, error) {
 	client := redisclient.NewClient(&redisclient.Options{
 		Addr: addr,
@@ -47,5 +38,13 @@ func (s *Store) IsTokenBlacklisted(ctx context.Context, tokenID string) (bool, e
 }
 
 func (s *Store) IncrementRateLimit(ctx context.Context, key string, window time.Duration) (int64, error) {
-	return rateLimitScript.Run(ctx, s.client, []string{key}, int(window.Seconds())).Int64()
+	pipe := s.client.TxPipeline()
+	incr := pipe.Incr(ctx, key)
+	pipe.Expire(ctx, key, window)
+
+	_, err := pipe.Exec(ctx)
+	if err != nil {
+		return 0, err
+	}
+	return incr.Val(), nil
 }
