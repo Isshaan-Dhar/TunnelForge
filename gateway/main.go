ac2db08d13ee2d25e6f3d99b89ebe4c6c1d016e7
Ishaan Dhar <ishaan.dhar2004@gmail.com>
2026-07-07 17:04:49 +0530

    Remove RealIP middleware from gateway

--- gateway/main.go
+++ gateway/main.go
@@ -64,7 +64,6 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(chimiddleware.RequestID)
-	r.Use(chimiddleware.RealIP)
 	r.Use(chimiddleware.Logger)
 	r.Use(chimiddleware.Recoverer)
 	r.Use(chimiddleware.Timeout(30 * time.Second))
