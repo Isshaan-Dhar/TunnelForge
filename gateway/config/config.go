bcca794e42769f80c316109ffccbd3b421772ace
Ishaan Dhar <ishaan.dhar2004@gmail.com>
2026-07-08 11:14:00 +0530

    Update config.go

--- gateway/config/config.go
+++ gateway/config/config.go
@@ -26,10 +26,15 @@ func Load() *Config {
 		log.Fatal("FATAL: INTERNAL_SECRET must be explicitly set to a secure string in the environment")
 	}
 
+	pgPass := os.Getenv("POSTGRES_PASSWORD")
+	if pgPass == "" || pgPass == "changeme" {
+		log.Fatal("FATAL: POSTGRES_PASSWORD must be explicitly set to a secure string in the environment")
+	}
+
 	return &Config{
 		AppPort:        getEnv("GATEWAY_PORT", "8443"),
 		MetricsPort:    "9090",
-		PostgresDSN:    "postgres://" + getEnv("POSTGRES_USER", "tunnelforge") + ":" + getEnv("POSTGRES_PASSWORD", "changeme") + "@" + getEnv("POSTGRES_HOST", "postgres") + ":" + getEnv("POSTGRES_PORT", "5432") + "/" + getEnv("POSTGRES_DB", "tunnelforge") + "?sslmode=disable",
+		PostgresDSN:    "postgres://" + getEnv("POSTGRES_USER", "tunnelforge") + ":" + pgPass + "@" + getEnv("POSTGRES_HOST", "postgres") + ":" + getEnv("POSTGRES_PORT", "5432") + "/" + getEnv("POSTGRES_DB", "tunnelforge") + "?sslmode=disable&pool_max_conns=50",
 		RedisAddr:      getEnv("REDIS_ADDR", "redis:6379"),
 		JWTSecret:      jwtSecret,
 		UpstreamURL:    getEnv("UPSTREAM_URL", "http://client-sim:9000"),
